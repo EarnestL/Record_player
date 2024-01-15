088b6fccc8b27ca32ca70e424cccf199b0e0c111
EarnestL <earnest.lin.2002@gmail.com>
2024-01-14 22:38:29 -0800

    build

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Record Player.X
+PROJECTNAME=Record_player
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Record_Player.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Record_Player.X.production.hex
+CND_ARTIFACT_NAME_default=Record_player.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Record_player.production.hex
